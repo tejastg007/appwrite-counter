88f16087101b704c332f5ff1bd7ff2c2f20295e9
tejas <tejastg007@gmail.com>
2025-08-24 16:49:31 +0530

    fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import SDK from 'node-appwrite'
+import { Client } from 'node-appwrite'
 
 
 export default async ({ req, res, log, error }) => {
-    const client = new SDK.Client()
+    const client = new Client()
         .setEndpoint(process.env.APPWRITE_API_ENDPOINT)
         .setProject(process.env.APPWRITE_PROJECT_ID)
         .setKey(process.env.APPWRITE_API_KEY)
